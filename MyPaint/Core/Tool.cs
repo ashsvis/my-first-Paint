edad6fd6975ec3aa057bbafc457097959b1663f6
v0v4 <44372426+v0v4@users.noreply.github.com>
2018-12-20 21:17:35 +0300

    created data model classes and a project for unit testing

---
+++ MyPaint/Core/Tool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPaint.Core
+{
+    /// <summary>
+    /// Tool to change layer
+    /// </summary>
+    public abstract class Tool
+    {
+        /// <summary>
+        /// Apply tool to layer
+        /// </summary>
+        internal virtual void Apply(Document doc)
+        {
+        }
+
+        /// <summary>
+        /// Render preview
+        /// </summary>
+        internal virtual void Render(Graphics gr, Document doc)
+        {
+        }
+    }
+}
