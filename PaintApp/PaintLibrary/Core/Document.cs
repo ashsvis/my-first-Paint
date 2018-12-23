465073704f7cebb158a210980e1aeea8775e5ecb
worldandlife <44372426+worldandlife@users.noreply.github.com>
2018-12-23 21:00:27 +0300

    Created new project

---
+++ PaintApp/PaintLibrary/Core/Document.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintLibrary.Core
+{
+    /// <summary>
+    /// Document
+    /// </summary>
+    public class Document
+    {
+        /// <summary>
+        /// Current tool
+        /// </summary>
+        public Tool Tool { get; protected set; }
+
+        /// <summary>
+        /// Current layer
+        /// </summary>
+        public Bitmap Layer { get; set; }
+
+        /// <summary>
+        /// Render layer
+        /// </summary>
+        public void Render(Graphics gr)
+        {
+            if (Tool != null)
+                Tool.Render(gr, this);
+            else
+                gr.DrawImage(Layer, Point.Empty);
+        }
+    }
+
+}
