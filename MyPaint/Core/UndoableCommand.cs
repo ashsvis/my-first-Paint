8e013eca7e0bf02f3eafcf81cb7702aeaf330f09
worldandlife <44372426+worldandlife@users.noreply.github.com>
2018-12-23 18:52:59 +0300

    Added UndoRedoManager

---
+++ MyPaint/Core/UndoableCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPaint.Core
+{
+    /// <summary>
+    /// Определение команды, обладающей возможностью отмены
+    /// </summary>
+    public abstract class UndoableCommand
+    {
+        public string Name { get; set; }
+        public abstract void Undo();
+        public abstract void Redo();
+    }
+}
